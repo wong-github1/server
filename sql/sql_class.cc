80104cf5dd0603bc3e437d8f0a8f8e98a71768f2
Andrei <andrei.elkin@mariadb.com>
2024-06-26 22:03:23 +0300

    MDEV-34481 optimize away waiting for owned by prepared xa non-unique index record
    This work partly implements a ROW binlog format MDEV-33455 part
    that is makes non-unique-index-only table XA replication safe in RBR.
    Existence of at least one non-null unique index has always guaranteed
    safety (no hang to error).
    
    Two transaction that update a non-unique-only index table could not be
    isolated on slave when on slave they used different non-unique indexes
    than on master.
    Unsolvable hang could be seen in case the first of the two is a prepared XA
    
    --connection slave_worker_1
    
      xa start 'xid'; /* ... lock here ... */ ; xa prepare 'xid'
    
    while the 2nd being of any kind including of normal type
    
    --connection slave_worker_2
    
      begin; /* ... get lock ... => wait/hang...error out  */
    
    was unable to wait up for the conflicting lock, even though
    the XA transaction did not really lock target records of the 2nd.
    
    This type of hang was caused by a chosen method the 2nd transaction
    employs to reach the target record, which is the index scan. The scanning
    orthodoxically just could not step over a record in the way that was
    locked by the XA.
    However as the in-the-way record can not be targeted by the 2nd
    transaction, otherwise the transactions would have sensed the conflict
    back on master *and* the other possibility of collecting extra locks by the
    'xid' on non-modified records is tacked by MDEV-33454/MDEV-34466,
    the non-unique index scanning server/slave-applier layer must not panic at
    seeing a timeout error from the engine. Instead the scanning would
    just proceed to next possibly free index records of the same key value and
    ultimately must reach the target one.
    More generally, on the way to its target all busy records belonging to
    earlier (binlog order) prepared XA transactions need not be tried locking
    by the current non-unique index scanning transaction.
    
    This patch implements the plan for Innodb.
    The server layer expects the engine to mark an attempt to wait for
    a conflicting lock that belongs to a transaction in prepared state.
    The engine won't exercise, need not to, the timeout wait.
    When marking is done the timeout error is ignored by the server
    and next index record is tried out.
    
    An mtr test checks a scenario in sequential and parallel modes.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -5418,7 +5418,9 @@ thd_need_wait_reports(const MYSQL_THD thd)
 /*
   Used by storage engines (currently InnoDB) to report that
   one transaction THD is about to go to wait for a transactional lock held by
-  another transactions OTHER_THD.
+  another transactions OTHER_THD. Also in case of a non-unique index locking
+  the slave applier is informed on a prepared XA transaction holding
+  an offending lock.
 
   This is used for parallel replication, where transactions are required to
   commit in the same order on the slave as they did on the master. If the
@@ -5432,6 +5434,9 @@ thd_need_wait_reports(const MYSQL_THD thd)
   slave in the first place. However, it is possible in case when the optimizer
   chooses a different plan on the slave than on the master (eg. table scan
   instead of index scan).
+  When the latter takes places and the conflict on a non-unique index involves
+  the slave applier the latter needs to know whether the lock onwer is
+  a prepared XA and this is provided.
 
   Storage engines report lock waits using this call. If a lock wait causes a
   deadlock with the pre-determined commit order, we kill the later
@@ -5444,20 +5449,33 @@ thd_need_wait_reports(const MYSQL_THD thd)
   transaction.
 */
 extern "C" int
-thd_rpl_deadlock_check(MYSQL_THD thd, MYSQL_THD other_thd)
+thd_rpl_deadlock_check(MYSQL_THD thd, MYSQL_THD other_thd,
+                       bool is_other_prepared, bool is_index_unique,
+                       uint *flagged)
 {
-  rpl_group_info *rgi;
-  rpl_group_info *other_rgi;
+  rpl_group_info *rgi=  thd ? thd->rgi_slave : NULL;
+  rpl_group_info *other_rgi= other_thd ? other_thd->rgi_slave : NULL;
 
   if (!thd)
     return 0;
   DEBUG_SYNC(thd, "thd_report_wait_for");
   thd->transaction->stmt.mark_trans_did_wait();
+
+  /*
+    Return to the caller the fact of the non-unique index wait lock
+    conflicts with one of a prepared state transaction.
+  */
+  if (!is_index_unique && rgi && rgi->is_row_event_execution()) {
+    if (is_other_prepared && !other_thd)
+    {
+      rgi->exec_flags |= 1 << rpl_group_info::HIT_BUSY_INDEX;
+      (*flagged)++;
+    }
+  }
+
   if (!other_thd)
     return 0;
   binlog_report_wait_for(thd, other_thd);
-  rgi= thd->rgi_slave;
-  other_rgi= other_thd->rgi_slave;
   if (!rgi || !other_rgi)
     return 0;
   if (!rgi->is_parallel_exec)
